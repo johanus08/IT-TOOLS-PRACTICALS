0fe22e2fbb8b04486b10dde6922947ae8d0ff98f
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 16:30:51 +0530

    Update practical 1A.py

--- practical 1A.py
+++ practical 1A.py
@@ -1,21 +1,28 @@
+# Python program to calculate area of different shapes
+# Importing math module
 import math
 
+# Class Rectangle created
 class Rectangle:
     def calculation(self,length,breadth):
         print(f"Area of the rectangle is {length * breadth}")
 
+# Class Square created
 class Square:
     def calculation(self,side):
         print(f"The area of the square is {side ** 2}")
 
+# Class Circle created       
 class Circle:
     def calculation(self,radius):
         print(f"The area of the circle is {math.pi * radius ** 2}")
-    
+
+# Class Ellipse created        
 class Ellipse:
     def calculation(self,x_axis,y_axis):
         print(f"The area of the Ellipse is {math.pi * x_axis * y_axis}")
 
+ # Calling objects
 a1 = Rectangle()
 a2 = Square()
 a3 = Circle()
