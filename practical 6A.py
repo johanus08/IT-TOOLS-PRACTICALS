95e28cbdd0e30c89d95bc6eef06dfb74d77240c4
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 17:00:46 +0530

    Update practical 6A.py

--- practical 6A.py
+++ practical 6A.py
@@ -1,20 +1,18 @@
-#class animal created
+# Class animal created
 class Animal:
 
     def speak(self):  
-
         print("Animal Speaking")  
 
 
-#child class Dog inherits the base class Animal  
+# Child class Dog inherits the base class Animal  
 class Dog(Animal):
   
     def bark(self):  
-
         print("dog barking")  
 
         
-#objects for animals
+# Objects for animals
 dog_object = Dog()  
 dog_object.bark()  
 dog_object.speak()  
