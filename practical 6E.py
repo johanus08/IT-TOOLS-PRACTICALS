89c4c120182497bbd4f28f42118a715dd8e5676d
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 17:08:42 +0530

    Update practical 6E.py

--- practical 6E.py
+++ practical 6E.py
@@ -1,17 +1,21 @@
+# Creating parent class
+# Class Parent created
 class Parent:
-    #creating parent class
-
+ 
     def func1(self):
         print("This is function one")
 
+# Class Child created
 class Child(Parent):
     def func2(self):
         print("This is function two")
 
+# Class Child1 created
 class Child1(Parent):
     def func3(self):
         print("This is function three")
 
+# Class Child# created
 class Child3(Child,Child1):
     def func4(Self):
         print("This is function four")
