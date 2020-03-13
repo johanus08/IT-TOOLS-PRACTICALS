0d449ef4583bfa8fa2055ddd42fef5c2d1923d68
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 16:33:04 +0530

    Update practical 1B.py

--- practical 1B.py
+++ practical 1B.py
@@ -1,4 +1,7 @@
+# Python program to perform arithmetic operations
 class Arithmetic_oprt:
+    
+    # Defining function for calculations
     def calculations(self,number1,number2):
         print(f"The addition of the numbers is :{number1 + number2}")
         print(f"The subtraction of the numbers is :{number1 - number2}")
@@ -8,5 +11,6 @@ class Arithmetic_oprt:
         print(f"The value of number 1 raised to number2 is {number1 ** number2}")
         print(f"The value of integer division of the numbers is {number1 // number2}")
 
+# Calling object 
 a1 = Arithmetic_oprt()
 a1.calculations(int(input("Enter number1:")),int(input("Enter number2:")))
