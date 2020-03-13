0df0f626a4340157076e9506fc2aab25d798b41a
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 16:35:54 +0530

    Update practical 2A.py

--- practical 2A.py
+++ practical 2A.py
@@ -1,41 +1,37 @@
+# Creating a class bank.
 class Bank:
-    #creating a class bank.
-
-    #accepting user details.
+    
+    # Accepting user details.
     user_name = str(input("Enter name:"))
     user_account_number = str(input("Enter account number:"))
     user_balance = 0
     
+    # Will deposit certain amount in the bank account.
     def deposit(self,deposit_amount):
-        #will deposit certain amount in the bank account.
-        
         Bank.user_balance += deposit_amount
         print(f"Curernt balance is :{ Bank.user_balance}")
-        
+    
+    # Will withdraw certain amonunt from the bank account.
     def withdraw(self,withdraw_amount):
-        #will withdraw certain amonunt from the bank account.
-        
         Bank.user_balance -= withdraw_amount
         print(f"Curernt balance is :{ Bank.user_balance}")
         
+    # Display user details.    
     def user(self):
-        #display user details.
-        
         print(f"The name is { Bank.user_name}")
         print(f'The account number is { Bank.user_account_number}')
         print(f"Curernt balance is :{ Bank.user_balance}")
         
 
-bank_object = Bank()        #creating an object for bank.
+bank_object = Bank()  # Creating an object for bank.
 
 while True:
-    
     print("1.User details.")
     print("2.Deposit amount.")
     print("3.Withdraw amount")
     print("4.Quit.")
-    #creating an option system to navigate through the options.
     
+    #creating an option system to navigate through the options.
     option = int(input("Enter option:"))
     if option == 2:
         bank_object.deposit(int(input("Enter deposit amount:")))
