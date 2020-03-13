df19eac215e98ac30e2f856ba1e05f2cf2c164c7
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 16:46:27 +0530

    Update practical 4A.py

--- practical 4A.py
+++ practical 4A.py
@@ -1,22 +1,19 @@
-#python function to fing the area of rectangle using constructor and destructor
-
+# Python function to fing the area of rectangle using constructor and destructor
+# Class Rectangle created
 class Rectangle:
 
-      #initializing the rectangle instance
-
+      # Initializing the rectangle instance
       def __init__ (self,l,b):
            print("Initializing an rectangle instance")
            self.l=l
            self.b=b
 
-      #calculating the area of rectangle
-           
+      # Calculating the area of rectangle   
       def area_r(self):
             area=self.l*self.b
             print("Area of rectangle is ",area)
 
-      #destroying the instance of rectangle
-
+      # Destroying the instance of rectangle
       def __del__(self):
          print("Instance is destroyed")
 
